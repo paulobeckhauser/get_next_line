5795cc1d4cf08c103b09a32617ff58e85826237d
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s1d1.42wolfsburg.de>
2023-12-09 18:00:02 +0100

    Updated version

--- get_next_line.c
+++ get_next_line.c
@@ -6,100 +6,28 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 12:00:01 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/12/08 19:16:53 by pabeckha         ###   ########.fr       */
+/*   Updated: 2023/12/09 15:20:41 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*ft_read(int fd, char *store)
-{
-	char	*buffer;
-	char	*temp;
-	int		read_bytes;
-
-	buffer = malloc(BUFFER_SIZE + 1);
-	if (!buffer)
-		return (NULL);
-	read_bytes = 1;
-	while (!ft_strchr(store, '\n') && read_bytes != 0)
-	{
-		read_bytes = read(fd, buffer, BUFFER_SIZE);
-		if (read_bytes == -1)
-			return (free(buffer), free(store), NULL);
-		buffer[read_bytes] = '\0';
-		temp = ft_strjoin(store, buffer);
-		free(store);
-		store = temp;
-	}
-	return (free(buffer), store);
-}
-
-char	*ft_line(char *store, int i)
-{
-	char	*str;
-
-	if (!store[i])
-		return (NULL);
-	while (store[i] && store[i] != '\n')
-		i++;
-	if (store[i] == '\n')
-		i++;
-	str = malloc(i + 1);
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (store[i] && store[i] != '\n')
-	{
-		str[i] = store[i];
-		i++;
-	}
-	if (store[i] == '\n')
-	{
-		str[i] = store[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-char	*ft_save_string(char *store)
-{
-	int		i;
-	int		j;
-	char	*str;
-
-	i = 0;
-	while (store[i] && store[i] != '\n')
-		i++;
-	if (!store[i])
-		return (free(store), NULL);
-	str = malloc((ft_strlen(store) - i + 1) * sizeof(char));
-	if (!str)
-		return (free(store), NULL);
-	i++;
-	j = 0;
-	while (store[i])
-		str[j++] = store[i++];
-	str[j] = '\0';
-	return (free(store), str);
-}
+#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
-	static char	*store;
-	char		*line;
-	int			i;
+	int			bytes_read;
+	char		*cup_buffer;
+	static int	count = 1;
 
-	i = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
-	if (!store)
-		store = ft_strdup("");
-	store = ft_read(fd, store);
-	if (!store)
+	printf("ft_calloc#[%d]---", count++);
+	cup_buffer = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!cup_buffer)
 		return (NULL);
-	line = ft_line(store, i);
-	store = ft_save_string(store);
-	return (line);
+	bytes_read = read(fd, cup_buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
+	{
+		free(cup_buffer);
+		return (NULL);
+	}
+	return (cup_buffer);
 }
--- main.c
+++ main.c
@@ -6,36 +6,35 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 18:59:00 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/12/08 19:17:20 by pabeckha         ###   ########.fr       */
+/*   Updated: 2023/12/09 15:16:14 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <fcntl.h> // open function
-#include <stdio.h> // printf function
+#include "get_next_line.h" //malloc(ft_calloc) and read functions
+#include <fcntl.h>         //open function
+#include <stdio.h>         //printf function
 
 int	main(void)
 {
 	int		fd;
-	char	*line;
+	char	*next_line;
 	int		count;
 
 	count = 0;
 	fd = open("example.txt", O_RDONLY);
 	if (fd == -1)
 	{
-		printf("Error opening file");
+		printf("Error opening File!");
 		return (1);
 	}
 	while (1)
 	{
-		line = get_next_line(fd);
-		if (line == NULL)
+		next_line = get_next_line(fd);
+		if (next_line == NULL)
 			break ;
 		count++;
-		printf("[%d]: %s\n", count, line);
-		free(line);
-		line = NULL;
+		printf("[%d]: %s\n", count, next_line);
+		free(next_line);
 	}
 	close(fd);
 	return (0);
